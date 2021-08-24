32dbc02c93e3b3e04f2276a3302ce2c62d28aaa9
michelsales <michelsales467@gmail.com>
2021-08-24 18:16:03 -0300

    use ref

--- Curso-React-Coder/hooks-coder/src/views/examples/UseRef.jsx
+++ Curso-React-Coder/hooks-coder/src/views/examples/UseRef.jsx
@@ -1,15 +1,57 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
-
+import React, { useEffect, useRef, useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 const UseRef = (props) => {
-    return (
-        <div className="UseRef">
-            <PageTitle
-                title="Hook UseRef"
-                subtitle="Retorna um objeto mutável com a propriedade .current!"
-            />
-        </div>
-    )
-}
+  const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
+  const count = useRef(0);
+  const myInput1 = useRef(null);
+  const myInput2 = useRef(null);
 
-export default UseRef
+  count.current = count.current + 1;
+
+  useEffect(() => {
+    count.current = count.current + 1;
+    myInput2.current.focus()
+}, [value1]);
+
+useEffect(() => {
+    count.current++;
+    myInput1.current.focus()
+  }, [value2]);
+
+  return (
+    <div className="UseRef">
+      <PageTitle
+        title="Hook UseRef"
+        subtitle="Retorna um objeto mutável com a propriedade .current!"
+      />
+      <SectionTitle title="Exercicio #01" />
+      <div className="center">
+        <div>
+          <span className="text">Valor: </span>
+          <span className="text">{value1}[</span>
+          <span className="text red">{count.current}</span>
+          <span className="text">]</span>
+        </div>
+        <input
+          ref={myInput1}
+          type="text"
+          className="input"
+          value={value1}
+          onChange={(e) => setValue1(e.target.value)}
+        />
+      </div>
+      <SectionTitle title="Exercicio #02" />
+      <input
+        ref={myInput2}
+        type="text"
+        className="input"
+        value={value2}
+        onChange={(e) => setValue2(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default UseRef;

2ba8df16c8d3fcb90362527e8262d97794da0527
michelsales <michelsales467@gmail.com>
2021-08-30 17:52:09 -0300

    redux coder projeto funcionando com redux constantes

--- Curso-React-Coder/redux/redux-simples/src/store/actions/numeros.js
+++ Curso-React-Coder/redux/redux-simples/src/store/actions/numeros.js
@@ -1,15 +1,16 @@
 //action creator ( criador de actions )
+import { NUM_MIN_ALTERADO, NUM_MAX_ALTERADO } from './actionTypes'
 
 export function alterarNumeroMinino(novoNumero) {
     return {
-        type: 'NUM_MIN_ALTERADO',
+        type: NUM_MIN_ALTERADO,
         payload: novoNumero
     }
 }
 
 export function alterarNumeroMaximo(novoNumero) {
     return {
-         type: 'NUM_MAX_ALTERADO',
+         type: NUM_MAX_ALTERADO,
          payload: novoNumero
     }
 }
--- Curso-React-Coder/redux/redux-simples/src/store/reducers/numbersReducers.js
+++ Curso-React-Coder/redux/redux-simples/src/store/reducers/numbersReducers.js
@@ -1,18 +1,19 @@
+import { NUM_MIN_ALTERADO, NUM_MAX_ALTERADO } from "../actions/actionTypes";
 
 const initialState = {
   min: 7,
   max: 31,
 };
 
-export default function numbersReducers(state = initialState, action){
+export default function numbersReducers(state = initialState, action) {
   switch (action.type) {
-    case "NUM_MIN_ALTERADO": {
+    case NUM_MIN_ALTERADO: {
       return {
         ...state,
         min: action.payload,
       };
     }
-    case "NUM_MAX_ALTERADO": {
+    case NUM_MAX_ALTERADO: {
       return {
         ...state,
         max: action.payload,
@@ -21,5 +22,4 @@ export default function numbersReducers(state = initialState, action){
     default:
       return state;
   }
-
 }

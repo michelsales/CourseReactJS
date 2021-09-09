21013b423a91938c1add690c1dfbfee6542cb582
michelsales <michelsales467@gmail.com>
2021-09-09 13:30:56 -0300

    redux reservas com redux alterando quantidade e mostrando reservas

--- Curso-React-Coder/redux-udemy/src/App.js
+++ Curso-React-Coder/redux-udemy/src/App.js
@@ -1,15 +1,21 @@
-import Routes from "./routes";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import Routes from "./routes";
 import Header from "./components/Header";
+
+import store from "./store";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes /> 
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <Routes />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
--- Curso-React-Coder/redux-udemy/src/pages/Reservas/index.js
+++ Curso-React-Coder/redux-udemy/src/pages/Reservas/index.js
@@ -1,22 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { MdDelete } from "react-icons/md";
 import "./style.css";
 
 export default function Reservas() {
+  const reserves = useSelector((state) => state.reserve);
+  console.log(reserves);
   return (
     <div>
-      <h1 className="title">Você solicitou 1 reservas</h1>
-      <div className="reserva">
-        <img
-          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
-          alt="Maceio"
-        />
-        <strong>Viagem Maceio 7 dias</strong>
-        <span>Quantidade: 2</span>
-        <button type="button" onClick={() => {}}>
-          <MdDelete size={20} color="#000" />
-        </button>
-      </div>
+      <h1 className="title">Você solicitou {reserves.length} reservas</h1>
+
+      {reserves.map((reserve) => (
+        <div className="reserva" key={reserve.id}>
+          <img
+            src={reserve.image}
+            alt={reserve.title}
+          />
+          <strong>{reserve.title}</strong>
+          <span>Quantidade: 2</span>
+          <button type="button" onClick={() => {}}>
+            <MdDelete size={20} color="#000" />
+          </button>
+        </div>
+      ))}
       <footer>
         <button type="button">Solicitar reservas</button>
       </footer>

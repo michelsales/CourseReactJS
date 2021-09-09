cc6dec4047005d519d69fbe32815dc136394d8aa
michelsales <michelsales467@gmail.com>
2021-09-09 08:55:40 -0300

    redux reservas projeto

---
+++ Curso-React-Coder/redux-udemy/src/App.js
@@ -0,0 +1,17 @@
+import Routes from "./routes";
+import { BrowserRouter } from "react-router-dom";
+
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes /> 
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ Curso-React-Coder/redux-udemy/src/pages/Home/index.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import api from "../../services/api";
+export default function Home() {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    async function loadApi() {
+      const response = await api.get('trips');
+      setTrips(response.data);
+
+      console.log(response.data);
+    }
+
+    loadApi();
+  }, []);
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
